2d4d5f68589a84f654804727886611a7a76ec293
lzhec <nikita090587@gmail.com>
2018-05-29 17:43:33 +0800

    .

--- aistenok/orders/admin.py
+++ aistenok/orders/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import *
+
+
+from django.contrib import admin
+from .models import *
+
+
+class StatusAdmin (admin. ModelAdmin):
+	list_display = [field.name for field in Status._meta.fields]
+	list_filter = ['status', 'is_active', 'created']
+	search_fields = ['customer_name', 'customer_phone', 'comments', 'created']
+
+	class Meta:
+		model = Status
+
+admin.site.register(Status, StatusAdmin)
+
+
+
+class OrderAdmin (admin. ModelAdmin):
+	list_display = [field.name for field in Order._meta.fields]
+	list_filter = ['status', 'created']
+	search_fields = ['customer_name', 'customer_phone', 'comments', 'created']
+
+	class Meta:
+		model = Order
+
+admin.site.register(Order, OrderAdmin)
+
+
+
+class ProductInOrderAdmin (admin. ModelAdmin):
+	list_display = [field.name for field in ProductInOrder._meta.fields]
+	list_filter = ['order', 'product', 'is_active', 'created']
+	search_fields = ['order', 'product', 'created']
+
+	class Meta:
+		model = ProductInOrder
+
+admin.site.register(ProductInOrder, ProductInOrderAdmin)
+
+
 
 
