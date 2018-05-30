c10b803690594911767cd660da729d752741c827
lzhec <nikita090587@gmail.com>
2018-05-30 22:20:13 +0800

    .

--- aistenok/orders/models.py
+++ aistenok/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django.db.models.signals import post_save
 
 # модели: заказы
 
@@ -34,7 +35,7 @@ class ShippingStatus(models.Model):
 
 
 class Order(models.Model):
-	total_price = models.DecimalField(max_digits=10, decimal_places=2, default=None) # total price of all products in order
+	total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0) # total price of all products in order
 	customer_name = models.CharField(max_length=128)
 	customer_email = models.CharField(max_length=50, blank=True, default=None)
 	customer_phone = models.CharField(max_length=30, blank=True, default=None)	
@@ -58,8 +59,8 @@ class ProductInOrder(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, default=None)
 	product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, default=None)
 	nmb = models.IntegerField(default=1)
-	product_price = models.DecimalField(max_digits=10, decimal_places=2, default=None)
-	total_price = models.DecimalField(max_digits=10, decimal_places=2, default=None) # nmb*product.price
+	product_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+	total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0) # nmb*product.price
 	is_active = models.BooleanField(default=True)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
@@ -68,5 +69,28 @@ class ProductInOrder(models.Model):
 		return '%s' % self.product.name
 
 	class Meta:
-		verbose_name = 'Товар'
-		verbose_name_plural = 'Товары'
+		verbose_name = 'Товар в заказе'
+		verbose_name_plural = 'Товары в заказе'
+
+	# переопределяем метод save для вычисления и сохранения стоимости всех единиц товара
+	def save(self, *args, **kwargs):
+		product_price = self.product.price
+		self.product_price = product_price
+		self.total_price = self.nmb * product_price	
+
+		super(ProductInOrder, self).save(*args, **kwargs)
+
+
+
+def product_in_order_postsave(sender, instance, created, **kwargs):
+	order = instance.order
+	all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+	order_total_price = 0
+	for item in all_products_in_order:
+		order_total_price += item.total_price
+	
+	instance.order.total_price = order_total_price
+	instance.order.save(force_update=True)
+
+
+post_save.connect(product_in_order_postsave, sender=ProductInOrder)
